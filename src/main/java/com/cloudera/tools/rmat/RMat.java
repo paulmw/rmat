bce64e92998e34f0bfc1b913364661263909ebac
Paul Wilkinson <paulw@cloudera.com>
2012-10-19 12:13:43 +0100

    Fixing types in driver class.

--- src/main/java/com/cloudera/tools/rmat/RMat.java
+++ src/main/java/com/cloudera/tools/rmat/RMat.java
@@ -3,7 +3,6 @@ package com.cloudera.tools.rmat;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -36,8 +35,8 @@ public class RMat implements Tool {
 		
 		job.setMapOutputKeyClass(Edge.class);
 		job.setMapOutputValueClass(NullWritable.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputKeyClass(Edge.class);
+		job.setOutputValueClass(NullWritable.class);
 		
 		job.setInputFormatClass(RMatInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
